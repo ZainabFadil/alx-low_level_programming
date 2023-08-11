37c4f3c371209d82df6742b59edfbcc6138b4222
zainab fadil <ugs.19687@ci.suez.edu.eg>
2023-08-10 19:10:15 -0700

    puts func

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/*
+ * this is the first program with c
+ * puts: this function prints the string
+ * Return 0: in the success case
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
