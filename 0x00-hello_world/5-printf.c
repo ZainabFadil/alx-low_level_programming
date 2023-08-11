bc0ede565ca2fabd0a5da0c1f326368cf15a74b1
zainab fadil <ugs.19687@ci.suez.edu.eg>
2023-08-10 19:27:36 -0700

    berry

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+/**
+ * main - prints phrase and the starting point
+ * Return: 0 in Success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
