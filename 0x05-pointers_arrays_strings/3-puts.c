70623d4d81092b9057ff09a7aa3ec480ee6ccc66
zainab fadil <ugs.19687@ci.suez.edu.eg>
2023-08-22 03:53:42 -0700

    puts string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - prints the given string followed by new line
+ * @str: string argument
+ * Return: void
+ */
+void _puts(char *str)
+{
+	/*the function puts print the string */
+	puts(str);
+}
