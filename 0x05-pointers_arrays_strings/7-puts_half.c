4cbf6e648b365eee25daff82e89e34b4115d3490
zainab fadil <ugs.19687@ci.suez.edu.eg>
2023-08-22 06:18:14 -0700

    last half of string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * puts_half - print the second half of the string
+ * @str: given argument
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int len = strlen(str);
+	int n, i;
+
+	if (len % 2 == 0)
+		n = len / 2;
+	else
+		n = (len - 1) / 2;
+
+	for (i = n; i <= len - 1; ++i)
+		putchar(str[i]);
+	puts("\n");
+}
