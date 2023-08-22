fa188dbb6dce472120fc750d4bfe3eae3b23b96f
zainab fadil <ugs.19687@ci.suez.edu.eg>
2023-08-22 02:47:39 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strlen - function to get the length of a string
+ * @s: is the parameter of type array of char
+ * Return: the length of the string of type integer
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	length = strlen(s);
+	return (length);
+}
