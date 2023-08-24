d91a63e671388d7e1028c1e6dd9ca050e8ab4853
ZainabFadil <ugs.19687@ci.suez.edu.eg>
2023-08-24 10:49:01 +0300

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+*_strcmp - connects 2 strings
+*@s1: first string
+*@s2: second string
+*Return: int
+*/
+int _strcmp(char *s1, char *s2)
+{
+	int r = strcmp(s1, s2);
+
+	return (r);
+}
