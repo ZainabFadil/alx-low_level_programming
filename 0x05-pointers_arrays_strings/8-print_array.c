f7b39680402cbfd1740eb0567459f84d67a34c7c
ZainabFadil <ugs.19687@ci.suez.edu.eg>
2023-08-25 10:29:27 +0300

    print arr

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,13 +9,18 @@
 */
 void print_array(int *a, int n)
 {
-	int i;
-
-	for (i = 0; i < n; ++i)
+	if (n <= 0)
+		printf("\n");
+	else
 	{
-		if (i == n - 1)
-			printf("%d\n", *(a + i));
-		else
-			printf("%d, ", *(a + i));
+		int i;
+
+		for (i = 0; i < n; ++i)
+		{
+			if (i == n - 1)
+				printf("%d\n", *(a + i));
+			else
+				printf("%d, ", *(a + i));
+		}
 	}
 }
