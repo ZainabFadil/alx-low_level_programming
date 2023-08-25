a7fa122b1ba98d245c48851ad8db5697653ce214
ZainabFadil <ugs.19687@ci.suez.edu.eg>
2023-08-25 13:13:46 +0300

    half put

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,12 +14,15 @@ void puts_half(char *str)
 
 	if (len == 0)
 		putchar('\n');
-	else if (len % 2 == 0)
-		n = (len / 2);
 	else
-		n = (len - 1) / 2;
+	{
+		if (len % 2 == 0)
+			n = (len / 2);
+		else
+			n = (len - 1) / 2;
 
-	for (i = n; i < len; ++i)
-		putchar(str[i]);
-	puts("\n");
+		for (i = n; i < len; ++i)
+			putchar(str[i]);
+		puts("\n");
+	}
 }
