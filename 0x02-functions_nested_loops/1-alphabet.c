683ea7ed8b7c6e72ca1ba78e3d9169c1675ece46
zainab fadil <ugs.19687@ci.suez.edu.eg>
2023-08-26 11:03:25 -0700

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints all alphabets
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; ++x)
+		_putchar(x);
+	_putchar('\n');
+}
