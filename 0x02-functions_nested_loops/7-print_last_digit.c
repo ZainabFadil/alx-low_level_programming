057314f410fde4360e49c65e861747938691219f
ZainabFadil <ugs.19687@ci.suez.edu.eg>
2023-08-27 14:02:20 +0300

    my commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*print_last_digit - prints the last digit
+*Return: int
+*@x: parameter
+*/
+int print_last_digit(int x)
+{
+	x %= 10;
+	return (x);
+}
