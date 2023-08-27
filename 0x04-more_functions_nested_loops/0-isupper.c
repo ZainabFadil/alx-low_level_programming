6f177e3d4ebf194030d90cdd014b662b1439f705
ZainabFadil <ugs.19687@ci.suez.edu.eg>
2023-08-27 21:00:26 +0300

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,17 +3,11 @@
 #include <ctype.h>
 
 /**
-* _isupper : checks the alphabet
+* _isupper - checks the alphabet
 * @c: parameter
 * Return: int
 */
 int _isupper(int c)
 {
-	int r;
-
-	if (c >= (int) 'A' && c >= (int) 'Z')
-		r = 0;
-	else
-		r = 1;
-	return (r);
+	return (isupper(c));
 }
