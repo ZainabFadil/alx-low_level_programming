29c5105fd24e3708d55e6e7f8eb33792c74178ab
ZainabFadil <ugs.19687@ci.suez.edu.eg>
2023-08-29 04:28:14 +0300

    message request

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_memcpy - copies memory area
+ * @dest: param
+ * @src: param
+ * @n: param
+ * @Return: char
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int idx;
+
+	for (idx = 0; idx < n; idx++)
+	{
+		dest[idx] = src[idx];
+	}
+
+	return (dest);
+}
