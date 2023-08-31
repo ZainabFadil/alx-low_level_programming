42c527f493de7553eeb7ad31caa8b4d71491016c
ZainabFadil <ugs.19687@ci.suez.edu.eg>
2023-08-31 16:01:45 +0300

    message

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * _sqrt_recursion - the sqrt of n
+ * @n: int param
+ * Return: int
+ */
+int _sqrt_recursion(int n)
+{
+	double x;
+
+	x = sqrt(n);
+	if ((int) x == (float) x)
+		return (x);
+	else
+		return (-1);
+}
