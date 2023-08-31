a7ceb7eae240ee9519a09dbb92daf083dbb18546
ZainabFadil <ugs.19687@ci.suez.edu.eg>
2023-08-31 13:44:21 +0300

    length using recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen_recursion - prints the string length
+ * @s: length of the string
+ * Return: int
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s+1));
+}
