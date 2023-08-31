e7392130e42e55d1d215eaa78701f117c039d5ce
ZainabFadil <ugs.19687@ci.suez.edu.eg>
2023-09-01 00:11:31 +0300

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,12 +9,22 @@
  */
 int _sqrt_recursion(int n)
 {
-	double x;
-
-	/*sqrt function */
-	x = sqrt(n);
-	if ((int) x == (float) x)
-		return (x);
-	else
+	if (n < 0)
 		return (-1);
+	return (_sqrt(n, 0));
+}
+
+/**
+ * _sqrt - calculate the square root
+ * @n: the first param
+ * @i: the second param
+ * Return: int
+ */
+int _sqrt(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (_sqrt(n, i + 1));
 }
