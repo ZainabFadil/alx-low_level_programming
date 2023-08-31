4626684b09f8eaba9cf589e5bf07918563ddaf7f
zainab fadil <ugs.19687@ci.suez.edu.eg>
2023-08-31 01:53:46 -0700

    puts

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - prints the given string
+ * @s: parameter string
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
