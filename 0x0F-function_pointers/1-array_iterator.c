ff7dde86fd6cf819277913e87150992246350665
ZainabFadil <ugs.19687@ci.suez.edu.eg>
2023-09-14 05:20:46 +0300

    first and second task

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * array_iterator - a function that executes a function given as a parameter
+ * @array: param
+ * @size: paaram
+ * @action: paraam
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	/*Recurrence */
+	unsigned int i;
+
+	if (!array || !action)
+		return;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
