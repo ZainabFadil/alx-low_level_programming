4effbb3e3b4e93fde2018f277cd24a2a92cc9867
ZainabFadil <ugs.19687@ci.suez.edu.eg>
2023-10-05 14:34:51 +0300

    ssecond task

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,20 +6,21 @@
  * create_file - creates a file
  * @filename: the file path
  * @text_content: the content that wanted to be copied
+ * Return: integer (1) in Success
  */
 int create_file(const char *filename, char *text_content)
 {
 	int opn, wt, i;
 
 	if (filename == NULL)
-		retturn (-1);
+		return (-1);
 	if (text_content != NULL)
 	{
 		for (i = 0; text_content[i];)
 			i++;
 	}
 	opn = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	wt = write(opn, text_content, len);
+	wt = write(opn, text_content, i);
 	if (opn == -1 || wt == -1)
 		return (-1);
 	close(opn);
